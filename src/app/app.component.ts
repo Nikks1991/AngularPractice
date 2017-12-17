e9a37819471396ce0198f480731cc2dd83702dfb
Rohit Ingle <ingle.rohit@gmail.com>
2017-12-17 09:54:00 +0530

    Component Interaction completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-first-component',
+  templateUrl: './app.component.html'
 })
+
 export class AppComponent {
-  title = 'app';
+  private wishList  = [
+    'Scuba Diving 123',
+    'Play TT',
+    'World Tour',
+    'Jungle Safari',
+    'Wildlife Photography',
+    'Sky Diving'
+  ];  // contains whish list (hard coded temporarily)
+
+
+  addWish(value) {
+    this.wishList.push(value);
+  }
+
+  deleteWish(index) {
+    console.log('value', index);
+    this.wishList.splice(index, 1);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-
-import { AppComponent } from './app.component';
-
+import {AppComponent} from './app.component';
+import {AddListComponent} from './components/app-add-list/app-add-list.component';
+import {AppHeaderComponent} from './components/app-header/app-header.component';
+import {AppWishListComponent} from './components/app-wish-list/app-wish-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [ AppComponent, AppHeaderComponent, AppWishListComponent, AddListComponent ],
+  imports: [ BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  exports: [],
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+
+}
